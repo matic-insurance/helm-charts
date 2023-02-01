6bedd5bb8855ab47f1ccdc8fc41ccc30f32e0757
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2023-02-01 12:03:09 +0200

    Add golden tests

---
+++ stable/rails/tests/golden/golden_test.go
@@ -0,0 +1,47 @@
+package golden
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type GoldenTestCase struct {
+    GoldenFileName string
+    Templates []string
+    ValuesFiles []string
+}
+
+func TestGoldenTemplates(t *testing.T) {
+	t.Parallel()
+
+	chartPath, err := filepath.Abs("../../")
+	require.NoError(t, err)
+
+	testCases := []GoldenTestCase {
+	    {
+	        GoldenFileName: "defaults/deployment-webserver.golden.yaml",
+	        Templates: []string{"templates/deployment-webserver.yaml"},
+	    },
+	    {
+	        GoldenFileName: "defaults/full.golden.yaml",
+	        ValuesFiles: []string{"defaults/values.golden.yaml"},
+	    },
+	    {
+	        GoldenFileName: "regular-app/full.golden.yaml",
+	        ValuesFiles: []string{"regular-app/values.golden.yaml"},
+	    },
+	}
+
+	for _, testCase := range testCases {
+		suite.Run(t, &GoldenTestSuite{
+			ChartPath:      chartPath,
+			Release:        "rails-test",
+			GoldenFileName: testCase.GoldenFileName,
+			Templates:      testCase.Templates,
+			ValuesFiles:    testCase.ValuesFiles,
+		})
+	}
+}
